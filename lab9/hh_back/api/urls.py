a61b530a742405b7f8e2d688d90a3e3a59786024
almirpernen <a_pernen@kbtu.kz>
2024-04-10 23:03:40 +0600

    Lab9

---
+++ lab9/hh_back/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import get_companies, get_company, get_vacancies, get_vacancy, get_company_vacancies, get_vacancies_top
+
+urlpatterns = [
+    path('companies/', get_companies),
+    path('companies/<int:company_id>', get_company),
+    path('companies/<int:company_id>/vacancies', get_company_vacancies),
+    path('vacancies/', get_vacancies),
+    path('vacancies/<int:vacancy_id>', get_vacancy),
+    path('vacancies/top_ten', get_vacancies_top)
+]
