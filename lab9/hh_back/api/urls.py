f94b026d6ef5cd3738607bc9a49184e41cd3ce6b
almirpernen <a_pernen@kbtu.kz>
2024-04-17 22:54:30 +0600

    finished

--- lab9/hh_back/api/urls.py
+++ lab9/hh_back/api/urls.py
@@ -4,9 +4,9 @@ from .views import get_companies, get_company, get_vacancies, get_vacancy, get_c
 
 urlpatterns = [
     path('companies/', get_companies),
-    path('companies/<int:company_id>', get_company),
-    path('companies/<int:company_id>/vacancies', get_company_vacancies),
+    path('companies/<int:company_id>/', get_company),
+    path('companies/<int:company_id>/vacancies/', get_company_vacancies),
     path('vacancies/', get_vacancies),
-    path('vacancies/<int:vacancy_id>', get_vacancy),
-    path('vacancies/top_ten', get_vacancies_top)
+    path('vacancies/<int:vacancy_id>/', get_vacancy),
+    path('vacancies/top_ten/', get_vacancies_top)
 ]
